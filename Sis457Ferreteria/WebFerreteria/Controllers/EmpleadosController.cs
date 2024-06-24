7e89984ace03d41ccea9eda7893673d783a10b2c
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-06-24 14:17:54 -0400

    Creación de la tabla Empleados y arreglo de sus interfaces

---
+++ Sis457Ferreteria/WebFerreteria/Controllers/EmpleadosController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebFerreteria.Models;
+
+namespace WebFerreteria.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private readonly LabFerreteriaContext _context;
+
+        public EmpleadosController(LabFerreteriaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Empleados
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Empleados.Where(x => x.Estado != -1).ToListAsync());
+        }
+
+        // GET: Empleados/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var empleado = await _context.Empleados
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            return View(empleado);
+        }
+
+        // GET: Empleados/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Empleados/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CedulaIdentidad,Nombres,PrimerApellido,SegundoApellido,Direccion,Celular,Cargo,UsuarioRegistro,FechaRegistro,Estado")] Empleado empleado)
+        {
+            if (!string.IsNullOrEmpty(empleado.CedulaIdentidad) && !string.IsNullOrEmpty(empleado.Nombres))
+            {
+                empleado.UsuarioRegistro = User.Identity.Name;
+                empleado.FechaRegistro = DateTime.Now;
+                empleado.Estado = 1;
+                _context.Add(empleado);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(empleado);
+        }
+
+        // GET: Empleados/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var empleado = await _context.Empleados.FindAsync(id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+            return View(empleado);
+        }
+
+        // POST: Empleados/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CedulaIdentidad,Nombres,PrimerApellido,SegundoApellido,Direccion,Celular,Cargo,UsuarioRegistro,FechaRegistro,Estado")] Empleado empleado)
+        {
+            if (id != empleado.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    empleado.UsuarioRegistro = User.Identity.Name;
+                    _context.Update(empleado);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmpleadoExists(empleado.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(empleado);
+        }
+
+        // GET: Empleados/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var empleado = await _context.Empleados
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            return View(empleado);
+        }
+
+        // POST: Empleados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var empleado = await _context.Empleados.FindAsync(id);
+            if (empleado != null)
+            {
+                empleado.Estado = -1;
+                empleado.UsuarioRegistro = User.Identity.Name;
+                await _context.SaveChangesAsync();
+                //_context.Empleados.Remove(empleado);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmpleadoExists(int id)
+        {
+            return _context.Empleados.Any(e => e.Id == id);
+        }
+    }
+}
